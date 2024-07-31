ca5fa9a6f7f59147aba349411ce544227fd7729d
Chemyth <ngovietanh121@gmail.com>
2024-07-31 17:58:35 +0700

    remove createDate method (using local time so it's not necessary)

--- src/main/java/com/anhngo/mainproject/controller/ProductsController.java
+++ src/main/java/com/anhngo/mainproject/controller/ProductsController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 @Controller
 @RequestMapping("/products")
 public class ProductsController {
@@ -47,6 +50,9 @@ public class ProductsController {
 
     @RequestMapping("/save")
     public String save(@ModelAttribute Product product) {
+        if (product.getCreateDate() == null) {
+            product.setCreateDate(LocalDate.now());
+        }
         productService.saveProduct(product);
         return "redirect:/products";
     }

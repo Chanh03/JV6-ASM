0be2fc30f1cdce3a2d0f727673f8427c120b034e
Anh Anh <ngovietanh121@gmail.com>
2024-08-28 13:14:14 +0700

    add env

--- src/main/java/com/anhngo/mainproject/repository/AccountRepo.java
+++ src/main/java/com/anhngo/mainproject/repository/AccountRepo.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepo extends JpaRepository<Account, String> {
     Page<Account> findAll(Pageable pageable);
